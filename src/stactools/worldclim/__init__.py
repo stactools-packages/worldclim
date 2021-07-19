1330bf9ecc5d2c7e713843eb78d27d214f32c1a9
cpapalaz <cpapalazarou@sparkgeo.com>
2021-07-19 11:16:37 -0700

    move version to init.py

--- src/stactools/worldclim/__init__.py
+++ src/stactools/worldclim/__init__.py
@@ -9,3 +9,6 @@ stactools.core.use_fsspec()
 def register_plugin(registry):
     from stactools.worldclim import commands
     registry.register_subcommand(commands.create_worldclim_command)
+
+__version__ = '0.1.5'
+"""Library version"""
