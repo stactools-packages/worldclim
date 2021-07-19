d7dd83600e7ec1ca525b1bb6a0a189aa40696521
cpapalaz <cpapalazarou@sparkgeo.com>
2021-07-19 09:30:33 -0700

    moved contents of worldclim-stactools to src/stactools/worldclim

--- src/stactools/worldclim/__init__.py
+++ src/stactools/worldclim/__init__.py
@@ -1,1 +1,11 @@
-__version__ = "0.1.0"
+import stactools.core
+
+from stactools.nrcanlandcover.stac import create_item
+from stactools.nrcanlandcover.cog import create_cog
+
+stactools.core.use_fsspec()
+
+
+def register_plugin(registry):
+    from stactools.nrcanlandcover import commands
+    registry.register_subcommand(commands.create_nrcanlandcover_command)
